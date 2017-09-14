dafa8ecbf5d1a8fb312854bfc5057e4b7f55d598
Daniel Winter <dev@danielwinter.at>
2017-09-14 14:05:13 +0200

    better font configuration for file-loader

--- webpack.config.js
+++ webpack.config.js
@@ -42,7 +42,7 @@ module.exports = {
                 })
             },
             {
-                test: /\.(png|gif|svg|eot|ttf|woff|woff2)$/,
+                test: /\.(svg|eot|ttf|woff|woff2)$/,
                 use: [
                     {
                         loader: 'url-loader',
